7203829b063de381d3b95d3b4bc30e01d08f0b0e
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-04-17 13:32:29 +0330

    add midelwer

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
 const express = require('express')
 const app = express()
 const path =require('path')
+const {urlincoded}= require('./middleware/md')
+const {json}=require('./middleware/md')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 app.get('/compose',(req,res)=>{
     res.sendFile(path.join(__dirname, '/compose.html'));
 })
 
+app.get('/home',(req,res)=>{
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+
 const PORT = 4000
 app.listen(PORT,()=>{
-    console.log('runing yor purt');
+    console.log('runing yor port');
 
 })
 
---
+++ middleware/md.js
@@ -0,0 +1,4 @@
+const { urlencoded } = require("express");
+
+// const urlincoded = require(urlencoded)
+const {json}= require('express')
