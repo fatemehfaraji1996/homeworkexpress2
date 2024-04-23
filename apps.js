f47da135dda0fa3b0f61d21ecd0bf6f5c15c5641
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-04-23 21:57:29 +0330

    add post req

--- apps.js
+++ apps.js
@@ -1,17 +1,19 @@
 const { isUtf8 } = require('buffer');
 const express = require('express');
 const { contentType, charset } = require('mime-types');
-// const path = require('path');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.get("/",  (req, res) => {
-  
- 
     res.sendFile(__dirname + "/index.html")
 });
+// 
 
-
+ app.post('/',(req,res)=>{
+    console.log(req.body);
+ })
+// 
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const formEle =document.querySelector('form')
 // console.log(formEle);
 console.log(",o,");
+formEle.addEventListener('submit',(e)=>{
+e.preventDefault()
+const inputNumbeer = document.querySelector('#one')
+const inputNumberSecend = document.querySelector('#tow')
+ let numberone= inputNumbeer.value
+ let numbetTow = inputNumberSecend.value
+
+
+ 
+})
