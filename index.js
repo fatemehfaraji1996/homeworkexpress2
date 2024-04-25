5006ff2f4e8a90c8bde0ec9a7fe92ebbab551d1d
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-04-25 20:39:32 +0330

    add index

---
+++ index.js
@@ -0,0 +1,15 @@
+// here you have a products array. by implementing express app write crud functions related to this array.
+//- get functions
+//- post
+//- put
+//- delete
+let products = [
+    { id: 1, name: 'iPhone 12 Pro', price: 1099.99 },
+    { id: 2, name: 'Samsung Galaxy S21', price: 999.99 },
+    { id: 3, name: 'Sony PlayStation 5', price: 499.99 },
+    { id: 4, name: 'MacBook Pro 16', price: 2399.99 },
+    { id: 5, name: 'DJI Mavic Air 2', price: 799.99 },
+  ];
+
+
+  
