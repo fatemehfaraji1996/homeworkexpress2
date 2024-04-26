491adb837ff8cf93d990ea69014d37e1cded7d4d
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-04-26 08:54:52 +0330

    end

--- index.js
+++ index.js
@@ -11,5 +11,41 @@ let products = [
     { id: 5, name: 'DJI Mavic Air 2', price: 799.99 },
   ];
 
+const { urlencoded } = require('body-parser');
+const exoress = require('express')
+const app = exoress()
+app.use(exoress.json())
+// app.use(urlencoded())
+const port = 4000
+// 
+app.get('/product',(req,res)=>{
+  res.send(res.json())
+})
+// get
+app.post('/product',(req,res)=>{
+  let {id,name,price} = req.body
+  const newData = req.body
+  req.body+= newData
+  res.json(newData)
+})
+// post
+app.put('product/:id',(req,res)=>{
+  const productId = parseInt(req.params.id) 
+  const updateProdact = req.body
+  products=products.map(product=>product.id ===productId ?updateProdact:product)
+  res.json(updateProdact)
 
+})
+// put
+app.delete('product/;id',(req,res)=>{
+  const deleteId = parseInt(req.params.id) 
+  products = products.filter(product => product.id !== deleteId)
+res.sendStatus(204)
+})
+
+
+
+app.listen(port,()=>{
+  console.log('your port is runing on ',port);
+})
   
